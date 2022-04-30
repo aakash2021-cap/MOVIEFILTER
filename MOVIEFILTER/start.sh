2347a2547ad9b60d6ebf5d9aa3837406e32d5687
aakash2021-cap <84575647+aakash2021-cap@users.noreply.github.com>
2022-04-30 10:22:18 +0530

    Update start.sh

--- MOVIEFILTER/start.sh
+++ MOVIEFILTER/start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/JOSProjects/IMDb-Movie-Bot.git /IMDb-Movie-Bot
-cd /IMDb-Movie-Bot
+git clone -b master https://github.com/aakash2021-cap/MOVIEFILTER.git /MOVIEFILTER
+cd /MOVIEFILTER
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
